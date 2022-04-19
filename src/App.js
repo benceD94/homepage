8ba971c5580ae07c433320b19a9e390d694661ad
Bence Dallos <bence.dallos@deliveryhero.com>
2022-04-19 18:00:50 +0200

    Make first template

--- src/App.js
+++ src/App.js
@@ -1,45 +1,60 @@
 import React, { useCallback } from 'react';
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
-import logo from './logo.svg';
-import './App.css';
 import particlesOptions from "./particles.json";
+import { makeStyles } from "@mui/styles";
+import { LinkedIn, MailOutline } from "@mui/icons-material";
+import {
+  Box,
+  Typography,
+  Link
+} from "@mui/material";
+
+const useStyles = makeStyles(() => ({
+  wrapper: {
+    color: "white",
+    position: "relative",
+    zIndex: 1,
+    width: "100vw",
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  links: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  icon: {
+    color: "white"
+  }
+}));
 
 function App() {
-    const particlesInit = useCallback(main => {
-        loadFull(main);
-    }, [])
+  const classes = useStyles();
+  const particlesInit = useCallback(main => {
+    loadFull(main);
+  }, [])
 
-    return (
-        <div className="App">
-            <Particles options={particlesOptions} init={particlesInit}/>
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <p>
-                    Edit <code>src/particles.json</code> to customize Particles, then save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                <a
-                    className="App-link"
-                    href="https://particles.js.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    See Particles samples
-                </a>
-            </header>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Particles options={particlesOptions} init={particlesInit}/>
+      <Box className={classes.wrapper}>
+        <Box>
+          <Typography variant="h1">Welcome</Typography>
+          <Box className={classes.links}>
+            <Link href="https://www.linkedin.com/in/bence-dallos/" target="_blank">
+              <LinkedIn fontSize="large" className={classes.icon} />
+            </Link>
+            <Link href="mailto:info@bence-dallos.com" target="_blank">
+              <MailOutline fontSize="large" className={classes.icon} />
+            </Link>
+          </Box>
+        </Box>
+      </Box>
+    </div>
+  );
 }
 
 export default App;
